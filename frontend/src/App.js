b11815fd5da2f2d7f70c90e153c0d943e66919b8
AnoyRC <38689344+AnoyRC@users.noreply.github.com>
2023-01-01 22:21:07 +0530

    Some trial program test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,10 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import {idl} from './idl'
 import { clusterApiUrl, Connection, Keypair, PublicKey, SystemProgram } from "@solana/web3.js";
-import { AnchorProvider, Program } from "@project-serum/anchor";
+import { AnchorProvider, Program, utils, web3 } from "@project-serum/anchor";
+import { BN } from "bn.js";
+import { Buffer} from "buffer";
+window.Buffer = Buffer;
 
 const CHAINLINK_FEED = "HgTtcbcmp5BeThax5AU8vg4VwK79qAvAKKFMs8txMLW6"
 const CHAINLINK_PROGRAM_ID = "HEvSKofvBgfaexv23kMabbYqxasxU3mQ4ibBMEmJWHny"
@@ -16,6 +19,9 @@ const opts = {
 
 const App = () => {
   const [walletAddress, setWalletAddress] = useState(null);
+  const [baseAccounts , setBaseAccounts] = useState();
+  const [baseAccount, setBaseAccount] = useState({});
+  const [registered, setRegistered] = useState(false);
 
   const checkIfWalletIsConnected = async () => {
     const { solana } = window;
@@ -32,6 +38,7 @@ const App = () => {
             response.publicKey.toString()
           );
           setWalletAddress(response.publicKey.toString());
+          getBaseAccounts()
         }
       } else {
         console.log("Solana Object not found !, get a Phantom Wallet");
@@ -46,12 +53,77 @@ const App = () => {
     const response = await solana.connect();
     console.log("Connected with public key : ", response.publicKey.toString());
     setWalletAddress(response.publicKey.toString());
+    getBaseAccounts()
   };
 
   const getProvider = () => {
     const connection = new Connection(network, opts.preFlightCommitment)
     const provider = new AnchorProvider(connection, window.solana , opts.preFlightCommitment)
     return provider;
+  }
+
+  const loginBaseAccount = async() => {
+    const provider = getProvider();
+    const baseAccount = baseAccounts.filter((account)=> account.admin.toString() === provider.wallet.publicKey.toString());
+    if(baseAccount[0] !== undefined){
+      setBaseAccount(baseAccount[0])
+      setRegistered(true)
+    }
+    else{
+      createBaseAccount();
+    }
+}
+
+  const getBaseAccounts = async() => {
+    const connection = new Connection(network, opts.preFlightCommitment);
+    const provider = getProvider();
+    const program = new Program(idl, programID, provider);
+    Promise.all(
+      (await connection.getProgramAccounts(programID,{
+        filters: [
+          {
+            dataSize: 200, 
+          },
+        ]})).map(
+        async (baseAccount) => ({
+          ...(await program.account.baseAccount.fetch(baseAccount.pubkey)),
+          pubkey: baseAccount.pubkey,
+        }) 
+      )
+    ).then((baseAccount)=> {
+      setBaseAccounts(baseAccount)
+      console.log(baseAccount)
+    })
+  }
+
+  const createBaseAccount = async() => {
+    try {
+      const provider = getProvider();
+      const program = new Program(idl,programID,provider);
+      const [baseAccount] = await PublicKey.findProgramAddressSync(
+        [
+          utils.bytes.utf8.encode("BASE_DEMO"),
+          provider.wallet.publicKey.toBuffer(),
+        ],
+        program.programId
+      )
+      await program.rpc.create({
+        accounts:{
+          baseAccount,
+          user : provider.wallet.publicKey,
+          systemProgram : SystemProgram.programId
+       },
+      })
+      console.log("Created a base account with address: ", baseAccount.toString())
+      const mainAccount = {
+        ...(await program.account.baseAccount.fetch(baseAccount)),
+        pubkey: baseAccount,
+      }
+      setBaseAccount(mainAccount)
+      setRegistered(true)
+    } catch (error) {
+      console.log("Error creating account", error)
+    }
   }
 
   const execute = async() => {
@@ -62,7 +134,7 @@ const App = () => {
       await program.rpc.execute({
         accounts:{
           resultAccount : resultAccount.publicKey,
-          user : provider.wallet.publicKey,
+          baseAccount : baseAccount.pubkey,
           systemProgram : SystemProgram.programId,
           chainlinkFeed: CHAINLINK_FEED,
           chainlinkProgram: CHAINLINK_PROGRAM_ID
@@ -76,12 +148,33 @@ const App = () => {
     }    
   }
 
+  const donate = async(baseAccount) => {
+    try {
+      const provider = getProvider()
+      const program = new Program(idl,programID,provider)
+      await program.rpc.donate(new BN(1 * web3.LAMPORTS_PER_SOL), {
+        accounts: {
+          baseAccount: baseAccount.pubkey,
+          user: provider.wallet.publicKey,
+          systemProgram: SystemProgram.programId
+        }
+      })
+      console.log("Successfully donated 0.2 sol to the blog creator")
+    } catch (error) {
+      console.log("Error Donating ", error)
+    }
+  }
+
   const renderNotConnectedContainer = () => {
     return <button onClick={connectWallet}> Connect Wallet </button>
   }
 
   const renderConnectedContainer = () => {
-    return <button onClick={execute}> Fetch Like Data </button>
+    return <div>
+        <button onClick={execute}> Fetch Live Data </button>
+        <button onClick={loginBaseAccount}> Login/ Signup </button>
+        <button onClick={()=>donate(baseAccount)}>Donate</button>
+      </div>
   }
 
   useEffect(()=>{
--- frontend/src/idl.js
+++ frontend/src/idl.js
@@ -1,58 +1,121 @@
 export const idl = {
-    "version": "0.1.0",
-    "name": "data_feed_dapp",
-    "instructions": [
-      {
-        "name": "execute",
-        "accounts": [
-          {
-            "name": "resultAccount",
-            "isMut": true,
-            "isSigner": true
-          },
-          {
-            "name": "user",
-            "isMut": true,
-            "isSigner": true
-          },
-          {
-            "name": "systemProgram",
-            "isMut": false,
-            "isSigner": false
-          },
-          {
-            "name": "chainlinkProgram",
-            "isMut": false,
-            "isSigner": false
-          },
-          {
-            "name": "chainlinkFeed",
-            "isMut": false,
-            "isSigner": false
-          }
-        ],
-        "args": []
-      }
-    ],
-    "accounts": [
-      {
-        "name": "ResultAccount",
-        "type": {
-          "kind": "struct",
-          "fields": [
-            {
-              "name": "value",
-              "type": "i128"
-            },
-            {
-              "name": "decimal",
-              "type": "u32"
-            }
-          ]
+  "version": "0.1.0",
+  "name": "data_feed_dapp",
+  "instructions": [
+    {
+      "name": "execute",
+      "accounts": [
+        {
+          "name": "resultAccount",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "baseAccount",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "systemProgram",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "chainlinkProgram",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "chainlinkFeed",
+          "isMut": false,
+          "isSigner": false
         }
-      }
-    ],
-    "metadata": {
-      "address": "BNK1JjB9vENSrinH4RJbSnLCJQrS9h1Mf21A4VjUh2a7"
+      ],
+      "args": []
+    },
+    {
+      "name": "create",
+      "accounts": [
+        {
+          "name": "baseAccount",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "user",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "systemProgram",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": []
+    },
+    {
+      "name": "donate",
+      "accounts": [
+        {
+          "name": "baseAccount",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "user",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "systemProgram",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "amount",
+          "type": "u64"
+        }
+      ]
     }
+  ],
+  "accounts": [
+    {
+      "name": "ResultAccount",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "value",
+            "type": "i128"
+          },
+          {
+            "name": "decimal",
+            "type": "u32"
+          }
+        ]
+      }
+    },
+    {
+      "name": "BaseAccount",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "admin",
+            "type": "publicKey"
+          },
+          {
+            "name": "amount",
+            "type": "u64"
+          }
+        ]
+      }
+    }
+  ],
+  "metadata": {
+    "address": "BNK1JjB9vENSrinH4RJbSnLCJQrS9h1Mf21A4VjUh2a7"
   }
+}
