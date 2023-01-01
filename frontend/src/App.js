a1e9b6d8604d325cf05c131182899d5d5352aea0
AnoyRC <38689344+AnoyRC@users.noreply.github.com>
2023-01-01 12:05:00 +0530

    Data feed per request Completion

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 import { useEffect, useState } from "react";
+import {idl} from './idl'
+import { clusterApiUrl, Connection, Keypair, PublicKey, SystemProgram } from "@solana/web3.js";
+import { AnchorProvider, Program } from "@project-serum/anchor";
+
+const CHAINLINK_FEED = "HgTtcbcmp5BeThax5AU8vg4VwK79qAvAKKFMs8txMLW6"
+const CHAINLINK_PROGRAM_ID = "HEvSKofvBgfaexv23kMabbYqxasxU3mQ4ibBMEmJWHny"
+
+const programID = new PublicKey(idl.metadata.address)
+const network = clusterApiUrl('devnet')
+
+const opts = {
+  preFlightCommitment: 'processed'
+}
 
 const App = () => {
   const [walletAddress, setWalletAddress] = useState(null);
@@ -35,12 +48,40 @@ const App = () => {
     setWalletAddress(response.publicKey.toString());
   };
 
+  const getProvider = () => {
+    const connection = new Connection(network, opts.preFlightCommitment)
+    const provider = new AnchorProvider(connection, window.solana , opts.preFlightCommitment)
+    return provider;
+  }
+
+  const execute = async() => {
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programID, provider);
+      const resultAccount = Keypair.generate()
+      await program.rpc.execute({
+        accounts:{
+          resultAccount : resultAccount.publicKey,
+          user : provider.wallet.publicKey,
+          systemProgram : SystemProgram.programId,
+          chainlinkFeed: CHAINLINK_FEED,
+          chainlinkProgram: CHAINLINK_PROGRAM_ID
+        },
+        signers:[resultAccount]
+      })
+      const latestPrice = await program.account.resultAccount.fetch(resultAccount.publicKey);
+      console.log(latestPrice.value.toString() / 100000000)
+    } catch (error) {
+      console.log('Error getting the feed', error)
+    }    
+  }
+
   const renderNotConnectedContainer = () => {
     return <button onClick={connectWallet}> Connect Wallet </button>
   }
 
   const renderConnectedContainer = () => {
-    
+    return <button onClick={execute}> Fetch Like Data </button>
   }
 
   useEffect(()=>{
