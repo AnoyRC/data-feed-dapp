70d44b5acc355ad58ac10eccbbe5751836b57bb5
AnoyRC <38689344+AnoyRC@users.noreply.github.com>
2023-01-02 12:10:02 +0530

    OCR feed added to get live data

--- programs/data-feed-dapp/src/lib.rs
+++ programs/data-feed-dapp/src/lib.rs
@@ -23,31 +23,6 @@ pub mod data_feed_dapp {
         result_account.decimal = u32::from(decimals);
         Ok(())
     }
-
-    pub fn create(ctx: Context<Create>) -> ProgramResult{
-        let base_account = &mut ctx.accounts.base_account;
-        base_account.admin = *ctx.accounts.user.key;
-        base_account.amount = 0;
-        Ok(())
-    }
-
-    pub fn donate(ctx: Context<Donate>, amount: u64) -> ProgramResult{
-        let ix = anchor_lang::solana_program::system_instruction::transfer(
-            &ctx.accounts.user.key(),
-            &ctx.accounts.base_account.key(),
-            amount
-        );
-        anchor_lang::solana_program::program::invoke(
-            &ix,
-            &[
-                ctx.accounts.user.to_account_info(),
-                ctx.accounts.base_account.to_account_info()
-            ]
-        );
-
-        (&mut ctx.accounts.base_account).amount += amount;
-        Ok(())
-    }
 }
 
 #[derive(Accounts)]
@@ -55,7 +30,7 @@ pub struct Execute<'info>{
     #[account(init, payer=base_account, space =100)]
     pub result_account: Account<'info, ResultAccount>,
     #[account(mut)]
-    pub base_account: Account<'info, BaseAccount>,
+    pub user: Signer<'info>,
     /// CHECK:
     #[account(address = system_program::ID)]
     pub system_program: AccountInfo<'info>,
@@ -65,36 +40,11 @@ pub struct Execute<'info>{
     pub chainlink_feed: AccountInfo<'info>
 }
 
-#[derive(Accounts)]
-pub struct Create<'info>{
-    #[account(init, payer = user, space = 200, seeds = ["BASE_DEMO".as_ref(), user.key().as_ref()],bump)]
-    pub base_account: Account<'info,BaseAccount>,
-    #[account(mut)]
-    pub user: Signer<'info>,
-    /// CHECK:
-    #[account(address = system_program::ID)]
-    pub system_program: AccountInfo<'info>
-}
-
-#[derive(Accounts)]
-pub struct Donate<'info>{
-    #[account(mut)]
-    pub base_account: Account<'info, BaseAccount>,
-    #[account(mut)]
-    pub user: Signer<'info>,
-    /// CHECK:
-    pub system_program: Program<'info, System>
-}
-
 #[account]
 pub struct ResultAccount{
     pub value:i128,
     pub decimal:u32
 }
 
-#[account]
-pub struct BaseAccount{
-    pub admin: Pubkey,
-    pub amount: u64
-}
+
 
